145d750b827024fa701dd8f4c4b0ec5ed20da756
장동겸 <jangelliot0404@dgu.ac.kr>
2025-03-29 03:18:39 +0900

    ⚙️ Setting - SpringBoot Setting (#4)
    * Setting/#3: setting springboot project
    
    * Setting/#3: GPS 도메인 구현

---
+++ src/main/java/com/dongkyeom/gps/emitter/emitter/controller/command/EmitterCommandV1Controller.java
@@ -0,0 +1,12 @@
+package com.dongkyeom.gps.emitter.emitter.controller.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/gps")
+public class EmitterCommandV1Controller {
+
+}
